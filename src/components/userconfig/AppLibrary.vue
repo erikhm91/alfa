ad36a1d5619718f209610d910d12161f66f7fc8a
Erik Houge Mathisen <erikhougem@gmail.com>
2019-12-29 21:17:03 +0100

    added modal popup when clicking bootstrap card, and new component AppModal which holds the content of the modal

--- src/components/userconfig/AppLibrary.vue
+++ src/components/userconfig/AppLibrary.vue
@@ -8,10 +8,14 @@
       </div>
     </div>
     <div class="row justify-content-center">
-      <!-- <div class="col-9"> -->
-      <!-- bootstrap cards -->
-      <!-- :img-src="imgurl(app.icon)" -->
-      <!-- img-src="../../../public/assets/menu-writeword.png"  doesn't work -->
+
+      <!-- Modal - triggered from click on card button -->
+      <div>
+        <b-modal ok-only id="unnr-modal" :title="'#'+selectedApp.id+' '+selectedApp.name">
+          <unnrAppModal :app="selectedApp"></unnrAppModal>
+        </b-modal>
+      </div>
+
       <b-card-group deck>
         <div v-for="(app, i) in appList" v-bind:key="i">
           <b-card
@@ -21,49 +25,58 @@
             img-top
             tag="article"
             style="max-width: 17rem;"
-            class="mb-2"
+            class="mb-2 text-center"
           >
-            <b-card-text>
+            <div class="text-left">
+              <hr>
               <p>{{app.desc}}</p>
-              <p>Læringsmål: {{app.goal}}</p>
-              <p>Læringsfase: {{app.phase}}</p>
-            </b-card-text>
+         
+            </div>
 
-              <!-- Consider implementing 'npm install bootstrap4-toggle' for a better framework for this -->
-              <div class="custom-control custom-switch">
-                <input :checked="getAppEnabled(app.id)" type="checkbox" class="custom-control-input" :id="i" 
-                @click="toggleApp(app.id)"
-                />
-                <label class="custom-control-label" :for="i">Aktiver/deaktiver</label>
-              </div>
+            <button class="btn btn-primary w-100"  @click="showModal(app)">Les mer</button>
+            <hr />
+            <div class="custom-control custom-switch text-center">
+            <!-- Consider implementing 'npm install bootstrap4-toggle' for a better tool for this   v-b-modal.modal -->
+              <input
+                :checked="getAppEnabled(app.id)"
+                type="checkbox"
+                class="custom-control-input"
+                :id="i"
+                @click="toggleApp(app.id)"/>
+              <label class="custom-control-label w-100" :for="i">Aktiver/deaktiver</label>
+            </div>
             
           </b-card>
         </div>
       </b-card-group>
-      <!-- <li class v-for="(app, i) in appList" v-bind:key="i">{{ app.appname }}</li> -->
-      <!-- </div> -->
     </div>
   </div>
 </template>
 
 <script>
 import ArrowBack from "@/components/icons/ArrowBack.vue";
+import AppModal from './AppModal.vue';
 // import AppListIcon from "@/components/icons/AppListIcon.vue";
 
 export default {
   data() {
     return {
-      letterClicked: false,
-      selectedLetter: {},
+      selectedApp: {},
       appList: this.$store.getters.appList
+
     };
   },
   components: {
     unnrArrowBack: ArrowBack,
+    unnrAppModal: AppModal
     // unnrAppListIcon: AppListIcon
   },
 
   methods: {
+    showModal(app)  {
+      this.selectedApp = app;
+      this.$bvModal.show('unnr-modal')
+    },
     printValue(value) {
       console.log("value: " + value);
     },
@@ -79,7 +92,7 @@ export default {
       });
     },
     getAppEnabled(appId) {
-      const index = this.appList.findIndex(obj => obj.id === appId)
+      const index = this.appList.findIndex(obj => obj.id === appId);
       if (this.appList[index].enabled == true) {
         return "checked";
       } else {
@@ -91,7 +104,6 @@ export default {
       //enable letter in store so it is visible in letterlist
       this.$store.commit("TOGGLE_ENABLE_APP", appId);
       console.log(this.appList);
-      
     }
   }
 };
