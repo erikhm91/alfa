c3695306639f9afd9fc9846561aa1b177eb6bd15
Erik Houge Mathisen <erikhougem@gmail.com>
2020-02-25 11:56:26 +0100

    reactivated animation after deactivating for test..

--- src/routes.js
+++ src/routes.js
@@ -40,7 +40,7 @@ export const routes = [/*
             store.commit("SET_TASK_IMAGES", loadedImages);
             setTimeout(() => {
                 next();
-            }, 0);
+            }, 2500);
             
         },
         beforeUpdate(to, from, next) {
